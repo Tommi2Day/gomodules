331647e65f4adc5113c10609cb89336a008f90bf
Tommi2Day <tommi2day@tommi2day.net>
2023-02-17 19:20:16 +0100

    refactor: mv ldap_test.go to module

--- ldap/go.mod
+++ ldap/go.mod
@@ -2,10 +2,16 @@ module github.com/tommi2day/gomodules/ldap
 
 go 1.19
 
-require github.com/go-ldap/ldap/v3 v3.4.4
+require (
+	github.com/go-ldap/ldap/v3 v3.4.4
+	github.com/stretchr/testify v1.7.2
+)
 
 require (
 	github.com/Azure/go-ntlmssp v0.0.0-20220621081337-cb9428e4ac1e // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- ldap/ldap_test.go
+++ ldap/ldap_test.go
@@ -1,8 +1,7 @@
-package test
+package ldap
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/tommi2day/gomodules/ldap"
 	"os"
 	"testing"
 )
@@ -12,9 +11,9 @@ func TestLdap(t *testing.T) {
 		t.Skip("Skipping LDAP testing in CI environment")
 	}
 	t.Run("Setup Config", func(t *testing.T) {
-		ldap.SetConfig("ldap.test", 0, true)
-		actual := ldap.GetConfig()
-		assert.IsType(t, ldap.ConfigType{}, actual, "Wrong type returned")
+		SetConfig("ldap.test", 0, true)
+		actual := GetConfig()
+		assert.IsType(t, ConfigType{}, actual, "Wrong type returned")
 		assert.Equal(t, "ldap.test", actual.Server, "Server not equal")
 		assert.Equal(t, 636, actual.Port, "with tls=true port should be 636")
 	})
