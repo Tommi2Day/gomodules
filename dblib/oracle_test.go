519cade9333806612a3c0045e0540ed65f2068ab
Tommi2Day <tommi2day@tommi2day.net>
2023-02-19 21:28:13 +0100

    refactor: oracle tests with docker

--- dblib/oracle_test.go
+++ dblib/oracle_test.go
@@ -6,10 +6,9 @@ import (
 	"fmt"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
+
 	ora "github.com/sijms/go-ora/v2"
 	"github.com/sijms/go-ora/v2/network"
 	"github.com/stretchr/testify/assert"
@@ -20,14 +19,10 @@ import (
 const DBUSER = "system"
 const DBPASSWORD = "XE-manager21"
 const TIMEOUT = 5
-const port = "21521"
-const repo = "docker.io/gvenzl/oracle-xe"
-const repoTag = "21.3.0-slim"
-const containerName = "dblib-oracle"
-const containerTimeout = 240
 const TESTDATA = "testdata"
 
 var dbhost = common.GetEnv("DB_HOST", "127.0.0.1")
+var oracleContainer *dockertest.Resource
 var connectora = fmt.Sprintf("XE.local=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=%s)(PORT=%s)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XEPDB1)))", dbhost, port)
 var target string
 
@@ -40,81 +35,8 @@ func makeOerr(code int, msg string) *network.OracleError {
 	return e
 }
 
-// prepareContainer create an Oracle Docker Container
-func prepareContainer() (pool *dockertest.Pool, dbContainer *dockertest.Resource, err error) {
-	dbContainer = nil
-	pool = nil
-	if os.Getenv("SKIP_ORACLE") != "" {
-		err = fmt.Errorf("skipping ORACLE Container in CI environment")
-		return
-	}
-	pool, err = dockertest.NewPool("")
-	if err != nil {
-		err = fmt.Errorf("cannot attach to docker: %v", err)
-		return
-	}
-	vendorImagePrefix := common.GetEnv("VENDOR_IMAGE_PREFIX", "")
-	repoString := vendorImagePrefix + repo
-
-	fmt.Printf("Try to start docker container for %s:%s\n", repoString, repoTag)
-	dbContainer, err = pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: repoString,
-		Tag:        repoTag,
-		Env: []string{
-			"ORACLE_PASSWORD=" + DBPASSWORD,
-		},
-		Name:         containerName,
-		ExposedPorts: []string{"1521"},
-		PortBindings: map[docker.Port][]docker.PortBinding{
-			"1521": {
-				{HostIP: "0.0.0.0", HostPort: port},
-			},
-		},
-	}, func(config *docker.HostConfig) {
-		// set AutoRemove to true so that stopped container goes away by itself
-		config.AutoRemove = true
-		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
-	})
-
-	if err != nil {
-		err = fmt.Errorf("error starting oracle docker container: %v", err)
-		return
-	}
-
-	pool.MaxWait = containerTimeout * time.Second
-	// hostAndPort = dbContainer.GetHostPort(port + "/tcp")
-	target = fmt.Sprintf("oracle://%s:%s@%s:%s/xepdb1", "system", DBPASSWORD, dbhost, port)
-	fmt.Printf("Wait to successfully connect to db with %s (max %ds)...\n", target, containerTimeout)
-	start := time.Now()
-	if err = pool.Retry(func() error {
-		var err error
-		var db *sql.DB
-		db, err = sql.Open("oracle", target)
-		if err != nil {
-			return err
-		}
-		return db.Ping()
-	}); err != nil {
-		fmt.Printf("Could not connect to DB Container: %s", err)
-		return
-	}
-	elapsed := time.Since(start)
-	fmt.Printf("DB Container is available after %s\n", elapsed.Round(time.Millisecond))
-	err = nil
-	return
-}
-func destroyContainer(pool *dockertest.Pool, dbContainer *dockertest.Resource) {
-	if err := pool.Purge(dbContainer); err != nil {
-		fmt.Printf("Could not purge resource: %s\n", err)
-	}
-}
-
 func TestWithOracle(t *testing.T) {
-	if os.Getenv("SKIP_ORACLE") != "" {
-		t.Skip("Skipping ORACLE testing in CI environment")
-	}
 	const alias = "XE.local"
-
 	tnsAdmin := TESTDATA
 	filename := tnsAdmin + "/connect.ora"
 	//_ = os.Chdir(tnsAdmin)
@@ -139,9 +61,14 @@ func TestWithOracle(t *testing.T) {
 	require.True(t, found, "Alias not found")
 	desc := common.RemoveSpace(e.Desc)
 
-	pool, dbContainer, err := prepareContainer()
-	require.NotNil(t, dbContainer, "Prepare failed")
-	defer destroyContainer(pool, dbContainer)
+	if os.Getenv("SKIP_ORACLE") != "" {
+		t.Skip("Skipping ORACLE testing in CI environment")
+	}
+
+	oracleContainer, err = prepareContainer()
+	require.NoErrorf(t, err, "Oracle Server not available:%v", err)
+	require.NotNil(t, oracleContainer, "Prepare failed")
+	defer destroyContainer(oracleContainer)
 
 	t.Run("Direct connect", func(t *testing.T) {
 		var db *sql.DB
