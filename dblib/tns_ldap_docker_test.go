ecc9db797c16beb251a55f947c484b710d50c9de
Tommi2Day <tommi2day@tommi2day.net>
2026-05-30 15:37:45 +0200

    update openldap container

--- dblib/tns_ldap_docker_test.go
+++ dblib/tns_ldap_docker_test.go
@@ -16,7 +16,7 @@ import (
 )
 
 const Ldaprepo = "docker.io/cleanstart/openldap"
-const LdaprepoTag = "latest"
+const LdaprepoTag = "2.6.13"
 const LdapcontainerTimeout = 120
 
 var TnsLdapcontainerName string
